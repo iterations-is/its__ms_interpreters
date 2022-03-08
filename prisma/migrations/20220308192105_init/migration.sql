0d4937973343aea16a9834b5db0fdf004ae2c433
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-03-08 20:23:28 +0100

    add: prisma init

---
+++ prisma/migrations/20220308192105_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Interpret" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "version" TEXT NOT NULL,
+    "url" TEXT NOT NULL,
+
+    CONSTRAINT "Interpret_pkey" PRIMARY KEY ("id")
+);
