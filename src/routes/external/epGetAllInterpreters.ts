c9be69645fea58976ec6f4a90d4a1d85c347beb1
Sergey Dunaevskiy <sergey@dunaevskiy.dev>
2022-03-19 14:07:21 +0100

    fix: rename interprets to interpreters

--- src/routes/external/epGetAllInterpreters.ts
+++ src/routes/external/epGetAllInterpreters.ts
@@ -5,9 +5,9 @@ const prisma = new PrismaClient();
 
 export const epGetAllInterpreters = async (req: Request, res: Response) => {
 	try {
-		const interprets = await prisma.interpret.findMany();
+		const interpreters = await prisma.interpret.findMany();
 
-		return res.status(200).json({ message: 'all interprets', payload: { interprets } });
+		return res.status(200).json({ message: 'all interpreters', payload: { interpreters } });
 	} catch (error) {
 		return res.status(500).json({ message: 'internal server error' });
 	}
